5e775f4c1f9ea08c4146f7ba12e7dbea58037c72
Виктория <99758968+sunshine-lllssi@users.noreply.github.com>
2025-11-27 20:46:21 +0000

    +

--- lab6/include/json.hpp
+++ lab6/include/json.hpp
@@ -32,12 +32,26 @@ namespace x3 = boost::spirit::x3;
 namespace json::types
 {
     //{ describe json data types
-    ... value = ...
+    struct value;
 
-    ... array
-    ... object
+    using array = std::vector<value>;
+    using object = std::map<std::string, value>;
 
-    ... json
+    using json = x3::variant<array, object>;
+
+    struct value: x3::variant<
+        nullptr_t,
+        bool,
+        int,
+        float,
+        std::string,
+        x3::forward_ast<array>,
+        x3::forward_ast<object>
+    >
+    {
+        using base_type::base_type;
+        using base_type::operator=;
+    };
     //}
 }
 
@@ -46,24 +60,27 @@ namespace json::parser
     const auto sfloat_ = x3::real_parser<float, x3::strict_real_policies<float>>();
 
     //{ describe json grammar
-    ... number = ...
-    ... nullable = ...
-    ... quated_string = ...
+    const auto number = sfloat_ | x3::int_;
+    const auto nullable = x3::lit("null") >> x3::attr(nullptr);
+    
+    x3::rule<struct quoted_string_class, std::string> const quoted_string = "quoted_string";
+    const auto quoted_string_def = x3::lexeme['"' >> *(('\\' > x3::char_) | ~x3::char_('"')) >> '"'];
 
-    ... array = ...
-    ... object = ...
-    ... json = ...
+    x3::rule<struct array_class, types::array> const array = "array";
+    x3::rule<struct object_class, types::object> const object = "object";
+    x3::rule<struct json_class, types::json> const json = "json";
+    x3::rule<struct value_class, types::value> const value = "value";
 
-    ... value = ...
+    const auto key_value = quoted_string >> ':' >> value;
 
-    ... key_value = ...
+    const auto array_def = '[' >> -(value % ',') >> ']';
+    const auto object_def = '{' >> -(key_value % ',') >> '}';
+    const auto json_def = array | object;
 
-    ... array??? = ...
-    ... object??? = ...
-    ... json??? = ...
+    const auto value_def = nullable | x3::bool_ | number | quoted_string | array | object;
     //}
 
-    BOOST_SPIRIT_DEFINE(array, object, json)
+    BOOST_SPIRIT_DEFINE(quoted_string, array, object, json, value)
 }
 
 #endif // __JSON_HPP__
